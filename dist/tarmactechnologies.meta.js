551316d6d55c93bf1cf82663869166b05060a4a2
Victor Colomb <victor@tarmactechnologies.com>
2025-03-13 15:30:52 +0100

    feat(agoa): metabase turnaround dashboard

--- dist/tarmactechnologies.meta.js
+++ dist/tarmactechnologies.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tarmac Technologies
-// @version      3.0.3
+// @version      3.1.0
 // @author       Victor Colomb
 // @description  Miscellaneous Tarmac Technologies Utilities
 // @icon         https://static-tarmac.s3.amazonaws.com/img/favicon.ico
